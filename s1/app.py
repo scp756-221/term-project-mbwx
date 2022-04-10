cb27398c3cdb66ee77432cb5e2c25505754cddb4
brijow <brijow@users.noreply.github.com>
2022-04-09 20:23:39 -0700

    cleanup app.py

--- s1/app.py
+++ s1/app.py
@@ -27,64 +27,65 @@ import simplejson as json
 app = Flask(__name__)
 
 metrics = PrometheusMetrics(app)
-metrics.info('app_info', 'User process')
+metrics.info("app_info", "User process")
 
-bp = Blueprint('app', __name__)
+bp = Blueprint("app", __name__)
 
 db = {
     "name": "http://cmpt756db:30002/api/v1/datastore",
-    "endpoint": [
-        "read",
-        "write",
-        "delete",
-        "update"
-    ]
+    "endpoint": ["read", "write", "delete", "update"],
 }
 
 
-@bp.route('/', methods=['GET'])
+@bp.route("/", methods=["GET"])
 @metrics.do_not_track()
 def hello_world():
-    return ("If you are reading this in a browser, your service is "
-            "operational. Switch to curl/Postman/etc to interact using the "
-            "other HTTP verbs.")
+    return (
+        "If you are reading this in a browser, your service is "
+        "operational. Switch to curl/Postman/etc to interact using the "
+        "other HTTP verbs."
+    )
 
 
-@bp.route('/health')
+@bp.route("/health")
 @metrics.do_not_track()
 def health():
     return Response("", status=200, mimetype="application/json")
 
 
-@bp.route('/readiness')
+@bp.route("/readiness")
 @metrics.do_not_track()
 def readiness():
     return Response("", status=200, mimetype="application/json")
 
 
-@bp.route('/<user_id>', methods=['PUT'])
+@bp.route("/<user_id>", methods=["PUT"])
 def update_user(user_id):
     headers = request.headers
     # check header here
-    if 'Authorization' not in headers:
-        return Response(json.dumps({"error": "missing auth"}), status=401,
-                        mimetype='application/json')
+    if "Authorization" not in headers:
+        return Response(
+            json.dumps({"error": "missing auth"}),
+            status=401,
+            mimetype="application/json",
+        )
     try:
         content = request.get_json()
-        email = content['email']
-        fname = content['fname']
-        lname = content['lname']
+        email = content["email"]
+        fname = content["fname"]
+        lname = content["lname"]
     except Exception:
         return json.dumps({"message": "error reading arguments"})
-    url = db['name'] + '/' + db['endpoint'][3]
+    url = db["name"] + "/" + db["endpoint"][3]
     response = requests.put(
         url,
         params={"objtype": "user", "objkey": user_id},
-        json={"email": email, "fname": fname, "lname": lname})
-    return (response.json())
+        json={"email": email, "fname": fname, "lname": lname},
+    )
+    return response.json()
 
 
-@bp.route('/', methods=['POST'])
+@bp.route("/", methods=["POST"])
 def create_user():
     """
     Create a user.
@@ -93,73 +94,72 @@ def create_user():
     """
     try:
         content = request.get_json()
-        lname = content['lname']
-        email = content['email']
-        fname = content['fname']
+        lname = content["lname"]
+        email = content["email"]
+        fname = content["fname"]
     except Exception:
         return json.dumps({"message": "error reading arguments"})
-    url = db['name'] + '/' + db['endpoint'][1]
+    url = db["name"] + "/" + db["endpoint"][1]
     response = requests.post(
-        url,
-        json={"objtype": "user",
-              "lname": lname,
-              "email": email,
-              "fname": fname})
-    return (response.json())
+        url, json={"objtype": "user", "lname": lname, "email": email, "fname": fname}
+    )
+    return response.json()
 
 
-@bp.route('/<user_id>', methods=['DELETE'])
+@bp.route("/<user_id>", methods=["DELETE"])
 def delete_user(user_id):
     headers = request.headers
     # check header here
-    if 'Authorization' not in headers:
-        return Response(json.dumps({"error": "missing auth"}),
-                        status=401,
-                        mimetype='application/json')
-    url = db['name'] + '/' + db['endpoint'][2]
-
-    response = requests.delete(url,
-                               params={"objtype": "user", "objkey": user_id})
-    return (response.json())
-
-
-@bp.route('/<user_id>', methods=['GET'])
-def get_user(user_id):
-    headers = request.headers
-    # check header here
-    if 'Authorization' not in headers:
+    if "Authorization" not in headers:
         return Response(
             json.dumps({"error": "missing auth"}),
             status=401,
-            mimetype='application/json')
+            mimetype="application/json",
+        )
+    url = db["name"] + "/" + db["endpoint"][2]
+
+    response = requests.delete(url, params={"objtype": "user", "objkey": user_id})
+    return response.json()
+
+
+@bp.route("/<user_id>", methods=["GET"])
+def get_user(user_id):
+    headers = request.headers
+    # check header here
+    if "Authorization" not in headers:
+        return Response(
+            json.dumps({"error": "missing auth"}),
+            status=401,
+            mimetype="application/json",
+        )
     payload = {"objtype": "user", "objkey": user_id}
-    url = db['name'] + '/' + db['endpoint'][0]
+    url = db["name"] + "/" + db["endpoint"][0]
     response = requests.get(url, params=payload)
-    return (response.json())
+    return response.json()
 
 
-@bp.route('/login', methods=['PUT'])
+@bp.route("/login", methods=["PUT"])
 def login():
     try:
         content = request.get_json()
-        uid = content['uid']
+        uid = content["uid"]
     except Exception:
         return json.dumps({"message": "error reading parameters"})
-    url = db['name'] + '/' + db['endpoint'][0]
+    url = db["name"] + "/" + db["endpoint"][0]
     response = requests.get(url, params={"objtype": "user", "objkey": uid})
     data = response.json()
-    if len(data['Items']) > 0:
-        encoded = jwt.encode({'user_id': uid, 'time': time.time()},
-                             'secret',
-                             algorithm='HS256')
+    if len(data["Items"]) > 0:
+        encoded = jwt.encode(
+            {"user_id": uid, "time": time.time()}, "secret", algorithm="HS256"
+        )
     return encoded
 
 
-@bp.route('/logoff', methods=['PUT'])
+@bp.route("/logoff", methods=["PUT"])
 def logoff():
     try:
         content = request.get_json()
-        _ = content['jwt']
+        _ = content["jwt"]
     except Exception:
         return json.dumps({"message": "error reading parameters"})
     return {}
@@ -168,13 +168,13 @@ def logoff():
 # All database calls will have this prefix.  Prometheus metric
 # calls will not---they will have route '/metrics'.  This is
 # the conventional organization.
-app.register_blueprint(bp, url_prefix='/api/v1/user/')
+app.register_blueprint(bp, url_prefix="/api/v1/user/")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) < 2:
         logging.error("Usage: app.py <service-port>")
         sys.exit(-1)
 
     p = int(sys.argv[1])
     # Do not set debug=True---that will disable the Prometheus metrics
-    app.run(host='0.0.0.0', port=p, threaded=True)
+    app.run(host="0.0.0.0", port=p, threaded=True)
